2b20d47dd6addcc59f9ad48c86b4cd0b889d930c
Minula Hansana <97427097+Minula20@users.noreply.github.com>
2022-02-24 01:42:51 +0530

    create registration form

---
+++ userlogon.php
@@ -0,0 +1,34 @@
+<html>
+    <head>
+        <title>User logging and registration</title>
+        <link rel="stylesheet" href="phpstyle.css">
+        <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+    </head>
+    <body>
+        <div class="container">
+            <div class="login-box">
+            <div class="row">
+                
+</div>
+                    <div class="col-md-6 login-right">
+                                        <h2>Register here</h2>
+                                        <form action="registration.php" method="post">
+                                            <div class="form-group">
+                                                <lable>Username</label>
+                                                <input type="text" name="user" class="form-control" required> 
+                    </div>
+                                                <div class="form-group">
+                                                <lable>Password</label>
+                                                <input type="password" name="password" class="form-control" required> 
+                    </div>                      <button type="submit" class="btn btn-primary"> Register </button>
+                    </form>
+                    </div>
+                    </div>
+</div>
+
+
+
+
+
+</body>
+</html>
